404aaef6dcd9cb90d949438478636b4e66716e72
NoNameNotAlive <104912449+NoNameNotAlive@users.noreply.github.com>
2022-05-27 02:32:00 +0200

    Isaac2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,13 +36,18 @@ namespace ABP
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.AutoSize = true;
             this.button1.Location = new System.Drawing.Point(497, 65);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 22);
+            this.button1.Size = new System.Drawing.Size(71, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,16 +59,18 @@ namespace ABP
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 64);
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(175, 64);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(374, 22);
+            this.textBox1.Size = new System.Drawing.Size(316, 22);
             this.textBox1.TabIndex = 1;
             // 
             // button2
             // 
+            this.button2.AutoSize = true;
             this.button2.Location = new System.Drawing.Point(574, 64);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 27);
             this.button2.TabIndex = 2;
             this.button2.Text = "Load";
             this.button2.UseVisualStyleBackColor = true;
@@ -71,9 +78,13 @@ namespace ABP
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(117, 176);
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(117, 181);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(451, 262);
+            this.richTextBox1.Size = new System.Drawing.Size(532, 263);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
@@ -82,23 +93,72 @@ namespace ABP
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(117, 114);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(212, 24);
             this.comboBox1.TabIndex = 4;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(279, 112);
+            this.comboBox2.Location = new System.Drawing.Point(335, 114);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.Size = new System.Drawing.Size(156, 24);
             this.comboBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(516, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "File:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Solidary At Home";
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(516, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Append";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(846, 456);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
@@ -107,6 +167,7 @@ namespace ABP
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "a";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +181,10 @@ namespace ABP
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -35,15 +35,12 @@ namespace ABP
             {
                 comboBox1.Items.Clear();
                 comboBox2.Items.Clear();
-                ReadFile();
+                ReadChilds();
 
             }
         }
-        private void ReadHost()
-        {
-           
-        }
-        private void ReadFile()
+
+        private void ReadChilds()
         {
             FileStream fs = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(textBox1.Text);
@@ -51,7 +48,6 @@ namespace ABP
             string linea;
             linea = sr.ReadLine();
             List<string> llista1 = new List<string>();
-            List<string> llista2 = new List<string>();
             while (linea != null)
             {
                 
@@ -63,8 +59,9 @@ namespace ABP
                         llista1.Add(GetElementName(linea));
 
                     }
-                }
-                
+                }         
+         
+
                 linea = sr.ReadLine();
 
             }
@@ -73,10 +70,6 @@ namespace ABP
             {
                 comboBox1.Items.Add(item);
             }
-            foreach (Object item in llista2)
-            {
-                comboBox2.Items.Add(item);
-            }
 
 
             sr.Close();
@@ -84,22 +77,198 @@ namespace ABP
         }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            comboBox2.Items.Clear();
+            if (comboBox1.SelectedItem.ToString().Equals("Hosts"))
+            {
+                GetFullNameHost();
+            }
+            if (comboBox1.SelectedItem.ToString().Equals("Refugees"))
+            {
+                GetFullNameRefugees();
+            }
+            if (comboBox1.SelectedItem.ToString().Equals("Foods"))
+            {
+                GetDescFood();
+            }
+            if (comboBox1.SelectedItem.ToString().Equals("FoodsDelivered"))
+            {
+                GetDeliveryNote();
+            }
 
 
-            
+
         }
-        private string[] GetValues()
+        private void GetFullNameRefugees()
+        {
+            FileStream fs = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(textBox1.Text);
+
+            string linea;
+            linea = sr.ReadLine();
+            List<string> llista2 = new List<string>();
+            while (linea != null)
+            {
+                while (!GetElementName(linea).Equals("Refugees"))
+                {
+
+                    linea = sr.ReadLine();
+
+                }
+                while (!GetElementName(linea).Equals("/Refugees"))
+                {
+                    if (GetElementName(linea).Equals("FullName"))
+                    {
+                        llista2.Add(GetElementData(linea));
+                        while (!(GetElementName(linea).Equals("/Refugee")) && linea != null)
+                        {
+                            linea = sr.ReadLine();
+                        }
+
+                    }
+                    linea = sr.ReadLine();
+                }
+
+                linea = null;
+
+            }
+            sr.Close();
+            fs.Close();
+            foreach (Object item in llista2)
+            {
+                comboBox2.Items.Add(item);
+            }
+        }
+        private void GetFullNameHost()
+        {
+            FileStream fs = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(textBox1.Text);
+
+            string linea;
+            linea = sr.ReadLine();
+            List<string> llista2 = new List<string>();
+            while (linea != null)
+            {
+                while (!GetElementName(linea).Equals("Hosts"))
+                {
+
+                    linea = sr.ReadLine();
+
+                }
+                while (!GetElementName(linea).Equals("/Hosts"))
+                {
+                    if (GetElementName(linea).Equals("FullName"))
+                    {
+                        llista2.Add(GetElementData(linea));
+                        while (!(GetElementName(linea).Equals("/Host")) && linea != null)
+                        {
+                            linea = sr.ReadLine();
+                        }
+
+                    }
+                    linea = sr.ReadLine();
+                }
+
+
+                
+                linea = null;
+               
+            }
+            sr.Close();
+            fs.Close();
+            foreach (Object item in llista2)
+            {
+                comboBox2.Items.Add(item);
+            }
+        }
+        private void GetDescFood()
         {
 
+            FileStream fs = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(textBox1.Text);
 
+            string linea;
+            linea = sr.ReadLine();
+            List<string> llista2 = new List<string>();
+            while (linea != null)
+            {
+                while (!GetElementName(linea).Equals("Foods"))
+                {
+
+                    linea = sr.ReadLine();
+
+                }
+                while (!GetElementName(linea).Equals("/Foods"))
+                {
+                    if (GetElementName(linea).Equals("DescFood"))
+                    {
+                        llista2.Add(GetElementData(linea));
+
+                    }
+                    linea = sr.ReadLine();
+                }
+
+
+
+                linea = null;
+
+            }
+            sr.Close();
+            fs.Close();
+            foreach (Object item in llista2)
+            {
+                comboBox2.Items.Add(item);
+            }
+
+        }
+        private void GetDeliveryNote()
+        {
+
+            FileStream fs = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(textBox1.Text);
+
+            string linea;
+            linea = sr.ReadLine();
+            List<string> llista2 = new List<string>();
+            while (linea != null)
+            {
+                while (!GetElementName(linea).Equals("FoodsDelivered"))
+                {
+
+                    linea = sr.ReadLine();
+
+                }
+                while (!GetElementName(linea).Equals("/FoodsDelivered"))
+                {
+                    if (GetElementName(linea).Equals("DeliveryNote"))
+                    {
+                        llista2.Add(GetElementData(linea));
+
+                    }
+                    linea = sr.ReadLine();
+                }
+
+
+
+                linea = null;
+
+            }
+            sr.Close();
+            fs.Close();
+            foreach (Object item in llista2)
+            {
+                comboBox2.Items.Add(item);
+            }
 
         }
         private string GetElementName(string linea)
         {
             string elementname = "";
-            string[] split = linea.Split('<', '>');
+            if(linea != null)
+            {
+                string[] split = linea.Split('<', '>');
 
-            elementname = split[1];
+                elementname = split[1];
+            }
             return elementname;
         }
         private string GetElementData(string linea)
@@ -110,6 +279,24 @@ namespace ABP
 
             data = split[2];
             return data;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione las listas porfavor");
+            }
+            
+        }
+        private void search()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
